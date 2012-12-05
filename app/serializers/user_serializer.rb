59efff466fdcbc672213ed22e8fee61f39493e02
croemmich <chris.roemmich@cojourners.com>
2012-12-05 04:38:39 -0600

    add person_id to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :id, :username, :locale, :primary_organization_id, :time_zone, :created_at, :updated_at
+  attributes :id, :username, :locale, :person_id, :primary_organization_id, :time_zone, :created_at, :updated_at
+  
+  def person_id 
+    object.person.id
+  end
   
   def primary_organization_id
     object.primary_organization_id
   end
   
-    def time_zone
+  def time_zone
     object.time_zone
   end
 
