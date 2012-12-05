1a5955a0224ccfe4a1eab4b70f32629e5fc4b857
croemmich <chris.roemmich@cojourners.com>
2012-12-05 01:13:40 -0600

    add primary to phone number serializer

--- app/serializers/phone_number_serializer.rb
+++ app/serializers/phone_number_serializer.rb
@@ -1,6 +1,6 @@
 class PhoneNumberSerializer < ActiveModel::Serializer
 
-  attributes :id, :person_id, :number, :location, :txt_to_email, :email_updated_at, :created_at, :updated_at
+  attributes :id, :person_id, :number, :location, :primary, :txt_to_email, :email_updated_at, :created_at, :updated_at
 
 end
 
