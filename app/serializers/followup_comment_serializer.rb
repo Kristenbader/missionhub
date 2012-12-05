ea71a666337658ec1b79783214a75407a47d9c53
croemmich <chris.roemmich@cojourners.com>
2012-12-05 00:35:03 -0600

    add organization id to followup comment serializer

--- app/serializers/followup_comment_serializer.rb
+++ app/serializers/followup_comment_serializer.rb
@@ -1,6 +1,6 @@
 class FollowupCommentSerializer < ActiveModel::Serializer
 
-  attributes :id, :contact_id, :commenter_id, :comment, :status,
+  attributes :id, :contact_id, :commenter_id, :comment, :status, :organization_id,
              :updated_at, :created_at, :deleted_at
 
 end
